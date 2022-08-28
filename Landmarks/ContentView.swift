ad8288aa203d61d5a2cdac43d2170e052b943289
Eric Liu <eric.liu.developer@gmail.com>
2022-08-27 22:50:40 -0700

    init1

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Eric Liu on 8/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+          
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+            
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewLayout(.device)
+            .previewDevice("iPhone 13 Pro Max")
+    }
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Eric Liu on 8/27/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
